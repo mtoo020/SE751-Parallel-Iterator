2192325cb8e15f35d3c886f636583d1a6f1bfbf6
kuyul <peyopeyo92@gmail.com>
2014-06-03 05:17:27 +1200

    Overlapping rectangles complete, although the code is not clean..

--- ParaIteratorCustomGroup7/src/MainDAG.java
+++ ParaIteratorCustomGroup7/src/MainDAG.java
@@ -1,4 +1,8 @@
+import java.awt.Color;
+import java.awt.Dimension;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.swing.*;
 
 import pi.GraphAdapterInterface;
 import pi.INode;
@@ -6,27 +10,63 @@ import pi.ParIterator;
 import pi.ParIterator.Schedule;
 import pi.ParIteratorFactory;
 
-public class MainDAG {
+public class MainDAG extends JFrame {
+	
+	static JLayeredPane d = new JLayeredPane();
+	static int rect1Count = 0;
+	static final int offset1 = 150;
+	static int rect2Count = 0;
+	static final int offset2 = 180;
+	static int rect3Count = 0;
+	static final int offset3 = 250;
+
+	
 	public static void main(String[] args) throws Exception {
 		int threadCount = 4;
 		int chunkSize = 1;
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				JFrame a = new JFrame("Overlapping images");
+				d.setVisible(true);
+
+//				JLabel label = new JLabel();
+//				label.setBackground(Color.red);
+//				label.setBounds(0, 0, 140, 140);
+//				label.setForeground(Color.black);
+//				label.setOpaque(true);
+//				d.add(label,1);
+
+				d.setPreferredSize(new Dimension(700, 500));
+				d.setBorder(BorderFactory.createTitledBorder(
+						"Move the Mouse to Move Duke"));
+				a.setSize(700, 700);
+
+
+				JComponent newContentPane = new JPanel();
+				newContentPane.add(d);
+				newContentPane.setVisible(true);
+				newContentPane.setOpaque(true); //content panes must be opaque
+				a.setContentPane(newContentPane);
+				a.setVisible(true);
+			}});
 
 		GraphAdapterInterface<INode, String> dag = new XLSImageParser("test4.xls").parse();
-		
+
 		long start = System.currentTimeMillis();
-		
+
 		@SuppressWarnings("unchecked")
 		ParIterator<INode> pi = ParIteratorFactory
-			.getTreeParIteratorBFSonDAGBottomTop(dag, dag.getStartNodes(), threadCount, chunkSize, Schedule.DYNAMIC, false);
-		
+		.getTreeParIteratorBFSonDAGBottomTop(dag, dag.getStartNodes(), threadCount, chunkSize, Schedule.DYNAMIC, false);
+
 		AtomicInteger atomicInt = new AtomicInteger(1);
-		
+
 		// Create and start a pool of worker threads
 		Thread[] threadPool = new WorkerThread[threadCount];
 		for (int i = 0; i < threadCount; i++) {
 			threadPool[i] = new WorkerThread(i, pi, atomicInt);
 			threadPool[i].start();
 		}
+
 
 		// ... Main thread may compute other (independent) tasks
 
@@ -38,9 +78,9 @@ public class MainDAG {
 				e.printStackTrace();
 			}
 		}
-		
+
 		long end = System.currentTimeMillis();
-		
+
 		System.out.println("All worker threads have completed.");
 		System.out.println("Time taken: "+(end - start)+" miliseconds");
 	}
--- ParaIteratorCustomGroup7/src/WorkerThread.java
+++ ParaIteratorCustomGroup7/src/WorkerThread.java
@@ -1,6 +1,10 @@
  
+import java.awt.Color;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
 import pi.INode;
 import pi.ParIterator;
@@ -21,22 +25,71 @@ public class WorkerThread extends Thread {
 	public void run() {
 		while (pi.hasNext()) {
 			INode element = pi.next();
+			String imageLink = ((ImageObject)element.getData()).getImageLink();
 			//System.out.println(atomicInt.getAndIncrement()+" Hello from Thread "+id+", who got node: " + element.getName() +" which has the data "+element.getData());
 			
-			System.out.println(atomicInt.getAndIncrement()+"| Thread "+id+" is given Element "+ element.getName()+" which has the data: "+((ImageObject)element.getData()).getImageLink());
+			System.out.println(atomicInt.getAndIncrement()+"| Thread "+id+" is given Element "+ element.getName()+" which has the data: "+imageLink);
 			
 			// slow down the threads (to illustrate the scheduling)
+			
 			try {
 				
 				Random rand = new Random();
 			    int randomNum = rand.nextInt((100 - 10) + 1) + 10;
 				
-				Thread.sleep(randomNum);
+				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+			
+			ImageIcon rect = createImageIcon(imageLink + ".png");
+			JLabel label = new JLabel(rect);
+			label.setOpaque(true);
+			System.out.println("image no " + imageLink.charAt(imageLink.length() - 1));
+			switch(imageLink.charAt(imageLink.length() - 1)){
+			case '0': 
+				label.setBounds(0, 0, rect.getIconWidth(), rect.getIconHeight());
+				MainDAG.d.add(label,0,0);
+				break;
+			case '1':
+				label.setBounds(15 + MainDAG.offset1 * MainDAG.rect1Count, 15, rect.getIconWidth(), rect.getIconHeight());
+				MainDAG.rect1Count++;
+				MainDAG.d.add(label,1,0);
+				break;
+			case '2':
+				label.setBounds(30 + MainDAG.offset2 * MainDAG.rect2Count, 50, rect.getIconWidth(), rect.getIconHeight());
+				MainDAG.rect2Count++;
+				MainDAG.d.add(label,2,0);
+				break;
+			case '3':
+				label.setBounds(60 + MainDAG.offset3 * MainDAG.rect3Count, 80, rect.getIconWidth(), rect.getIconHeight());
+				MainDAG.rect3Count++;
+				MainDAG.d.add(label,3,0);
+				break;
+			case '4':
+				label.setBounds(230, 120, rect.getIconWidth(), rect.getIconHeight());
+				MainDAG.d.add(label,4,0);
+				break;
+			default :
+				System.out.println("shouldn't be poping up");
+				break;
+
+			
+			}
+			
+			
 		}
 		System.out.println("    Thread "+id+" has finished.");
 	}
+	
+    private ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = WorkerThread.class.getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
 }
 
